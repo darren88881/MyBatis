0179a8031a2d4c1d0802b6c0375ee3e878c40b9c
darren88881 <darren88881@outlook.com>
2020-11-20 01:21:37 +0800

    mybatis初体验

---
+++ src/test/java/itguigu/com/AppTest.java
@@ -0,0 +1,34 @@
+package itguigu.com;
+
+import com.atguigu.mybatis.pojo.Employee;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    private SqlSessionFactory sqlSessionFactory;
+
+    @Before
+    public void getSqlSessionFactory() throws IOException {
+        InputStream resource = Resources.getResourceAsStream("mybatis-config.xml");
+        sqlSessionFactory = new SqlSessionFactoryBuilder().build(resource);
+
+    }
+
+    @Test
+    public void test01(){
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        Employee emp = sqlSession.selectOne("myEmployee.selectEmp", 1);
+        System.out.println(emp);
+    }
+}
